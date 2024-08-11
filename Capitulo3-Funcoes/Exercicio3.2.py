9b0d9e0aa8a350e3cb2e7e20913d7e153cac924a
e-brendon <brendonmontteiro@gmail.com>
2024-08-11 14:46:22 -0400

    ex2 up

---
+++ Capitulo3-Funcoes/Exercicio3.2.py
@@ -0,0 +1,11 @@
+'''
+Exercicio pede para criaar uma função que recebe um valor atribuido a uma variável ou passado como argumento.
+Por exempli: do_twice é uma função que toma um objeto defunção como argumento e chama dyas vezes
+'''
+
+def do_twice (f):
+    print(type(f))
+    print(type(f))
+
+varr = float(5) # fiz com variavel para trocar tipo de dado como foi printado na func para mostrar  tipo de dado usado no script
+do_twice(varr)
---
+++ Capitulo3-Funcoes/Teste_cap/testes.py
@@ -0,0 +1,18 @@
+# library import 
+import math 
+# criações das funções
+# primeira func testada no livro, e foi testada rapidamente, nem sei se está escrito corretamente.
+
+def print_lyrics():
+    print("I'm work at all day.")
+    print("I'm not work today with python ")
+
+# segunda func testada no livro 
+
+def print_twice(bruce):
+    print (bruce)
+    print (bruce)
+
+#chamada das funções para o teste
+#print_lyrics()
+print_twice('string-teste ' * 2)
